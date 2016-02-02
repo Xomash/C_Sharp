ef4cc7d0a21ef6328a311b96ee84038aec5c7c0b
Xomash <xoma02@gmail.com>
2016-02-02 15:32:49 +0200

    Lab01

---
+++ LabKhomandiak01/LabKhomandiak01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabKhomandiak01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = Initialize();
+            Console.WriteLine("So we have such a students:");
+            students.Sort((Student student1, Student student2) =>
+            student1.GetMiddle().CompareTo(student2.GetMiddle()));
+            int n = 0;
+            foreach (Student s in students)
+            {
+                Console.WriteLine(++n+(") ")+s.ToString());
+            }
+            Console.ReadKey();
+        }
+
+        public static List<Student> Initialize()
+        {
+            Student a = new Student("Khomandiak", 74, 98, 91, 95);
+            Student b = new Student("Yanivskiy", 91, 97, 83, 89);
+            Student c = new Student("Betsko", 61, 65, 63, 69);
+            Student d = new Student("Tverdokhlib", 61, 91, 81, 85);
+            Student e = new Student("Klymentovska", 75, 92, 82, 93);
+            List<Student> students = new List<Student>();
+            students.Add(a);
+            students.Add(b);
+            students.Add(c);
+            students.Add(d);
+            students.Add(e);
+            return students;
+        }
+    }
+}
---
+++ LabKhomandiak01/LabKhomandiak01/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabKhomandiak01
+{
+    class Student
+    {
+        private String surname { get; set; }
+        private  int obdz { get; set; }
+        private  int csharp { get; set; }
+        private  int android { get; set; }
+        private  int prolog { get; set; }
+        private  int middle { get; set; }
+
+        public int GetMiddle()
+        {
+            return middle;
+        }
+
+        public void SetMiddle(int middle)
+        {
+            this.middle = middle;
+        }
+
+        public Student (String surname, int obdz, int csharp, int android, int prolog)
+        {
+            this.surname = surname;
+            this.obdz = obdz;
+            this.csharp = csharp;
+            this.android = android;
+            this.prolog = prolog;
+            this.middle = (obdz + csharp + android + prolog) / 4;
+           // Console.WriteLine("Student " + surname + " was initialized");
+        }
+
+        override public String ToString()
+        {
+            return ("Student " + surname + " with marks: obdz(" + obdz + "), c#(" + csharp + "), android(" + android + "), prolog(" + prolog + "),"+
+                " average("+middle+").");
+        }
+    }
+}
