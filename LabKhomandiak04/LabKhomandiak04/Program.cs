01fe61cec6fe98ac18c84e0c3754926f989cd56d
Xomash <xoma02@gmail.com>
2016-02-24 18:22:21 +0200

    LabKhomandiak04

---
+++ LabKhomandiak04/LabKhomandiak04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabKhomandiak04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = IPGlobalProperties.GetIPGlobalProperties();
+            var b = a.GetActiveTcpConnections();
+            foreach(var c in b)
+            {
+                Console.WriteLine(c.LocalEndPoint.Port.ToString() +" "+ c.State.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
