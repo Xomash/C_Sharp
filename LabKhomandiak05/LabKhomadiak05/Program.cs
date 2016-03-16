e392f821f3ede428ca006033bcc45ab6793ec12f
Xomash <xoma02@gmail.com>
2016-03-16 21:55:44 +0200

    LabKhomandiak05

---
+++ LabKhomandiak05/LabKhomadiak05/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace LabKhomandiak03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var students = initialize();
+            binarySerialize(students);
+
+            IEnumerable<Student> iStudent = students.Where(a => a.averageMark > 86).Select(s => s).OrderBy(c=>c.course);
+            Console.WriteLine(iStudent.Count());
+            Console.WriteLine("Ordered by course");
+            foreach (Student s in iStudent)
+                Console.WriteLine(s);
+            iStudent = iStudent.OrderBy(n => n.pib).ThenBy(q => q.averageMark);
+            Console.WriteLine("Ordered by PIB");
+            foreach (Student s in iStudent)
+                Console.WriteLine(s);
+            Student[] hello = iStudent.ToArray();
+            int k = (int)iStudent.Select(i => i.averageMark).Average();
+            Console.WriteLine(iStudent.SequenceEqual(students));
+            Console.WriteLine(k);
+            Console.ReadKey();
+        }
+
+
+        static List <Student> initialize()
+        {
+            var students = new List<Student>();
+            students.Add(new Student("Oleh Khomandiak", 3, 90));
+            students.Add(new Student("Oleh Yanivskiy", 2, 93));
+            students.Add(new Student("Denis Tverdokhlib", 1, 72));
+            students.Add(new Student("Roman Betsko", 0, 61));
+            students.Add(new Student("Dana Klymentovska", 3, 84));
+            students.Add(new Student("Mikhaela Toporivska", 4, 87));
+            return students;
+        }
+        static void binarySerialize(List<Student> students)
+        {
+            Stream fsStream = new FileStream("E:\\Projects\\serialization.dat", FileMode.OpenOrCreate);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fsStream, students);
+            fsStream.Close();
+            Stream openStream = new FileStream("E:\\Projects\\serialization.dat", FileMode.Open);
+            List<Student> ss = (List<Student>)bf.Deserialize(openStream);
+            openStream.Close();
+        }   
+    }
+}
---
+++ LabKhomandiak05/LabKhomadiak05/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabKhomandiak03
+{
+    [Serializable]
+public class Student
+    {   
+        public String pib { get; set; }
+        public int course { get; set; }
+        public int averageMark { get; set; }
+        [NonSerialized]
+        public int hash;
+
+        public int GetHash()
+        {
+            return hash;
+        }
+
+        public void setHash(int value)
+        {
+            hash = value;
+        }
+
+        public Student(String pib, int course, int averageMark)
+        {
+            this.pib = pib;
+            this.course = course;
+            this.averageMark = averageMark;
+            hash = generate();
+        }
+
+        public Student()
+        {
+
+        }
+
+        public int generate()
+        {
+            return (pib.Length/3+course+6)*Math.Abs(averageMark);
+        }
+
+        override public String ToString()
+        {
+            return ("Student " + pib + " from " + course + " course, with average mark: " + averageMark+", secret function is:"+hash);
+        }
+
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            hash = generate();
+        }
+    }
+}
